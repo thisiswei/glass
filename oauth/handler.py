80824c4016047815af7c49170aaba437f51269d3
berk atikoglu <berkatikoglu@gmail.com>
2013-08-24 16:18:46 -0400

    changed welcome message

--- oauth/handler.py
+++ oauth/handler.py
@@ -131,7 +131,7 @@ class OAuthCodeExchangeHandler(OAuthBaseRequestHandler):
 
     # Insert welcome message.
     timeline_item_body = {
-        'text': 'Welcome to the Python Quick Start',
+        'text': 'Welcome to Stylr',
         'notification': {
             'level': 'DEFAULT'
         }
